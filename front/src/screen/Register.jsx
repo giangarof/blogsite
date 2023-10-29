61b64a4dc9a2f732ec46cd4f1ffd02dfaa8222de
Gianmarco G <giangarof98@gmail.com>
2023-10-28 23:15:34 -0400

    signin/signup form

--- front/src/screen/Register.jsx
+++ front/src/screen/Register.jsx
@@ -1,7 +1,22 @@
+import { Box, TextField, Stack, Button } from "@mui/material";
+import { Typography } from "@mui/joy";
+
 export default function Register() {
         return(
             <>
-              Register
+              <Box sx={{mt: 10}}>
+                <Stack direction="column" justifyContent="center" alignItems="center" spacing={4}>
+                    <Typography level="h3">New user information</Typography>
+
+                    <TextField id="outlined-basic" label="Full Name" variant="outlined" />
+                    <TextField id="outlined-basic" label="Username" variant="outlined" />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" />
+                    <TextField id="outlined-basic" label="Password" variant="outlined" />
+
+                    <Button variant="contained" size="large">Complete registration</Button>
+                </Stack>
+
+              </Box>
             </>
         )
 }
