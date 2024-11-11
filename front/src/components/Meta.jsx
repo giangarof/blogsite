853faa3687f98b01b58c157650a006e58f739fa9
Gianmarco G <giangarof98@gmail.com>
2024-11-11 14:00:30 -0500

    open graph

--- front/src/components/Meta.jsx
+++ front/src/components/Meta.jsx
@@ -5,8 +5,12 @@ export default function Meta({title,description, keywords}) {
   return (
     <Helmet>
         <title>{title}</title>
-        <meta name="description" content={description} />
+        <meta  name="description" content={description} />
         <meta name="keywords" content={keywords} />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
     </Helmet>
   )
 }
