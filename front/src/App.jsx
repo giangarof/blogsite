dc61e3d11cbb4b32e0fadcb85d52e0f615f1a2b2
Gianmarco G <giangarof98@gmail.com>
2025-01-19 16:46:55 -0500

    added useEffect

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from './components/Navbar.jsx'
 import ReactGA from "react-ga4";
